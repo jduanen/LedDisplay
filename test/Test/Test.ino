1d9e57a9ce646fccbb9cee78184e09dae1fd1925
Duane Northcutt <duane.northcutt@gmail.com>
2023-03-21 18:44:47 -0700

    Checkpoint.

--- test/Test/Test.ino
+++ test/Test/Test.ino
@@ -4,8 +4,12 @@
  * 
  ****************************************************************************/
 
-#define LEDS_ON_DELAY       100   // 100 usec
-#define INTER_ROW_DELAY     1000  // 1 msec
+#define ROW_DELAY           1000    // 1 msec (1KHz refresh rate)
+#define INTER_ROW_DELAY     (ROW_DELAY - ledsOnDelay)
+
+#define MAX_LEDS_ON_DELAY   0                 //  0 msec (0% brightness)
+#define DEF_LEDS_ON_DELAY   (ROW_DELAY / 2)   // .5 msec (50% brightness)
+#define MIN_LEDS_ON_DELAY   ROW_DELAY         //  1 msec (100% brightness)
 
 #define ROW_0               D0
 #define ROW_1               D1
@@ -46,6 +50,7 @@ byte frameBuffers[NUM_BUFS][NUM_ROWS][NUM_COLS] = {};
 byte curRow = 0;
 byte refreshBufNum = NONE;
 byte renderBufNum = NONE;
+uint32_t ledsOnDelay = DEF_LEDS_ON_DELAY;
 uint32_t loopCnt = 0;
 uint32_t loop1Cnt = 0;
 
@@ -102,9 +107,8 @@ void patternBuf(uint32_t bufNum, uint32_t pattNum) {
     case CHECK_PATTERN:
         for (uint32_t row = 0; (row < NUM_ROWS); row++) {
             for (uint32_t col = 0; (col < NUM_COLS); col++) {
-                frameBuffers[bufNum][row][col] = ((col + (row % (NUM_COLORS + 1))) % (NUM_COLORS + 1));
+                frameBuffers[bufNum][row][col] = ((col + (row % (NUM_COLORS + 2))) % (NUM_COLORS + 2));
             }
-            Serial.println("");
         }
         break;
     default:
@@ -113,17 +117,25 @@ void patternBuf(uint32_t bufNum, uint32_t pattNum) {
     }
 };
 
+void setBrightness(byte percent) {
+    // int <newvalue> = map(<value>, <original_min>, <original_max>, <new_min>, <new_max>);
+    assert((percent >= 0) && (percent <= 100));
+    ledsOnDelay = map(percent, 0, 100, MAX_LEDS_ON_DELAY, MIN_LEDS_ON_DELAY);
+}
+
+byte getBrightness() {
+    // int <newvalue> = map(<value>, <original_min>, <original_max>, <new_min>, <new_max>);
+    return map(ledsOnDelay, MAX_LEDS_ON_DELAY, MIN_LEDS_ON_DELAY, 0, 100);
+}
+
 void setup() {
     Serial.begin(115200);
-    delay(1000);
+    delay(1000);  // N.B. this delay value is necessary for correct function
     Serial.println("\nBEGIN: Render Loop");
-
     Serial.println("READY: Render Loop");
-    Serial.println(">" + String(rp2040.fifo.available()));
 };
 
 void setup1() {
-    delay(1500);
     Serial.println("\nBEGIN: Refresh Loop");
     pinMode(ROW_0, OUTPUT);
     pinMode(ROW_1, OUTPUT);
@@ -140,15 +152,16 @@ void setup1() {
     pinMode(COL_CLOCK, OUTPUT);
     digitalWrite(COL_CLOCK, LOW);
 
-    /*
     // all FB bufs start off as free
     for (uint32_t bufNum = 0; (bufNum < NUM_BUFS); bufNum++) {
         rp2040.fifo.push(bufNum);
     }
-    */
+    setBrightness(50);
+    delay(1500);  // N.B. this delay has to be here and in this amount
 
-    Serial.println("READY: Refresh Loop");
-    Serial.println(">>" + String(rp2040.fifo.available()));
+    Serial.print("READY: Refresh Loop; ");
+    Serial.print("Brightness: " + String(getBrightness()) + "%, ");
+    Serial.println(String(ledsOnDelay) + " / " + String(INTER_ROW_DELAY));
 };
 
 void enableRow(int rowColor, int rowNum) {
@@ -196,37 +209,49 @@ void shiftInPixels(int bufNum, int row, int color) {
 
 uint32_t pNum = 0;
 
+#define TEST_MODE       FIXED_PATTERN
+//#define TEST_MODE       CYCLE_PATTERNS
+#define FIXED_PATTERN   0
+#define CYCLE_PATTERNS  1
+
 // Render loop
 void loop() {
-    //// TMP TMP TMP just use check pattern
-    if (loopCnt == 0) {
-        for (renderBufNum = 0; (renderBufNum < NUM_BUFS); renderBufNum++) {
-            patternBuf(renderBufNum, CHECK_PATTERN);
-            rp2040.fifo.push(renderBufNum);
+    switch (TEST_MODE) {
+    case FIXED_PATTERN:
+        // fill all bufs with chosen pattern at the start
+        if (loopCnt == 0) {
+            for (renderBufNum = 0; (renderBufNum < NUM_BUFS); renderBufNum++) {
+//                patternBuf(renderBufNum, CHECK_PATTERN);
+                patternBuf(renderBufNum, ALL_AMBER_PATTERN);
+                rp2040.fifo.push(renderBufNum);
+            }
         }
+        break;
+    case CYCLE_PATTERNS:
+        if ((loopCnt % 3) == 0) {  // try to generate new pattern every third loop
+            uint32_t bNum;
+            if (rp2040.fifo.pop_nb(&bNum)) {
+                renderBufNum = bNum;
+                pNum = ((pNum + 1) % NUM_PATTERNS);
+                patternBuf(renderBufNum, pNum);
+                rp2040.fifo.push(renderBufNum);
+            }
+        }
+        break;
+    default:
+        // no pattern
+        break;
     }
 
-    //// TMP TMP TMP
-/*
-    if ((loopCnt % 5) == 0) {
-//        Serial.print("Render: Pattern: " + String(pNum) + ", ");
-//        Serial.print("avail: " + String(rp2040.fifo.available()) + ", ");
-        renderBufNum = rp2040.fifo.pop();  // blocking
-//        Serial.println("renderBufNum: " + String(renderBufNum));
-        pNum = (pNum + 1) % NUM_PATTERNS;
-        patternBuf(renderBufNum, pNum);
-        rp2040.fifo.push(renderBufNum);
-    }
-*/
     loopCnt++;
-    delay(1000);
+    delay(100);
 }
 
 // Refresh loop
 void loop1() {
     if (curRow == 0) {
         while (rp2040.fifo.available() > 0) {
-//            Serial.print("Refresh: avail: " + String(rp2040.fifo.available()) + ", ");
+//            Serial.print("Refresh: avail=" + String(rp2040.fifo.available()) + ", ");
             if ((refreshBufNum >= 0) && (refreshBufNum < NUM_BUFS)) {
                 rp2040.fifo.push(refreshBufNum);
 //                Serial.print("returned: " + String(refreshBufNum) + ", ");
@@ -240,52 +265,9 @@ void loop1() {
         assert((refreshBufNum >= 0) && (refreshBufNum < NUM_BUFS));
         shiftInPixels(refreshBufNum, curRow, colors[color]);
         enableRow(colors[color], curRow);
-        delayMicroseconds(LEDS_ON_DELAY);
+        delayMicroseconds(ledsOnDelay);
         disableRows();
     }
     curRow = (curRow + 1) % NUM_ROWS;
     delayMicroseconds(INTER_ROW_DELAY);
-
-/*
-    //// TMP TMP TMP
-    loop1Cnt++;
-    if ((loop1Cnt % 1000) == 0) {
-        Serial.println("Refresh: refreshBufNum:: " + String(refreshBufNum));
-    }
-*/
 };
-
-/*
-// can't toggle wires faster than 667nsec, so don't need any of this
-unsigned long hrTime;
-
-#define LOOP_CNT    1000000.0
-
-// N.B. This isn't all that accurate, especially with smaller input values
-#define nsecToLoopCnt(n)     ((n - 100) / 84)
-// measured time: ((l * 84) + 100) nsec
-#define spinLoop(l)             { volatile int _i; for(_i=0; _i<l; ++_i) {} }
-#define spinNanoseconds(nsec)   spinLoop(nsecToLoopCnt(nsec))
-
-    // Measurements
-    digitalWrite(COL_DATA, LOW);
-    for (int i = 0; (i < 200); i += 10) {
-        digitalWrite(COL_STROBE, LOW);  // 683nsec to get here from start of loop
-        spinLoop(i);    //
-        spinNanoseconds(i); // cnt: usec; 100: .950, 200: 1.0, 300: 1.1, 400: 1.67, 500: 1.27,... 900: 1.67
-                            // cnt: nsec; 0: 925, 40: 933..., 150: 933, 160: ?, 170: 916, 180: 925, 190: 1000
-        digitalWrite(COL_STROBE, HIGH); // loop overhead: ~700nsec
-    }
-    digitalWrite(COL_DATA, HIGH);
-    digitalWrite(COL_CLOCK, LOW); // 666.7nsec from above low to this high
-    delayMicroseconds(4);  // 1: 72.5usec, 2: 2.4usec, 3: 3.6usec
-    digitalWrite(COL_CLOCK, HIGH);
-};
-
-//        hrTime = micros();
-//        for (int j = 0; (j < LOOP_CNT); j++) {
-//        }
-//        float hrt = (micros() - hrTime) / LOOP_CNT;
-//        Serial.print(String(i) + ": ");
-//        Serial.println(hrt);
-*/
