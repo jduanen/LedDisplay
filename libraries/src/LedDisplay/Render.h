d53e3dea63c5fd082d4ddc4c2706f1fff6149882
Duane Northcutt <duane.northcutt@gmail.com>
2023-04-03 16:54:26 -0700

    Added setPixel() and show() methods.

--- libraries/src/LedDisplay/Render.h
+++ libraries/src/LedDisplay/Render.h
@@ -9,27 +9,30 @@
 
 #include "Refresh.h"
 
+//// TMP TMP TMP
+#define NUM_BUFS            3
+#define NUM_COLS            ((8 + 8 + 1) * 5)
+#define NUM_ROWS            7
 
 #define BLACK_COLORLIGHTS   0b00000000
 #define GREEN_COLORLIGHTS   0b00000001
 #define RED_COLORLIGHTS     0b00000010
 #define AMBER_COLORLIGHTS   (GREEN_COLOR | RED_COLOR)
 
-#define NUM_BUFS            3
 
-#define NONE                -1
-
-
-class Render: Refresh {
+class Render: public Refresh {
 public:
-    void setPixel(byte col, byte row, byte color);
+    Render();
+
+    void setPixel(uint8_t col, uint8_t row, uint8_t color);
     void show();
-protected:
-    byte _frameBuffers[NUM_BUFS][NUM_ROWS][NUM_COLS] = {};
-    ?
+
 private:
-    byte _bufferNum = NONE;
-    ?
+    uint8_t _renderBufNum = -1;
+
 };
+
+
+#include "Render.hpp"
 
 #endif // _LED_DISPLAY_RENDER_H_
