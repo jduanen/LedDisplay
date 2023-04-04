4694f209aab5ee81d7477743c966db94b01b58b8
Duane Northcutt <duane.northcutt@gmail.com>
2023-04-04 12:48:55 -0700

    Moved Render.hpp to Render.cpp, removed Render.hpp.

--- libraries/src/LedDisplay/Render.cpp
+++ libraries/src/LedDisplay/Render.cpp
@@ -4,3 +4,21 @@
  * 
  ****************************************************************************/
 
+Render::Render() {
+    Serial.println("Render");
+};
+
+void Render::setPixel(uint8_t col, uint8_t row, uint8_t color) {
+    if (!BUF_NUM_VALID(_renderBufNum)) {
+        _renderBufNum = rp2040.fifo.pop();
+//        Serial.println("_renderBufNum: " + String(_renderBufNum));
+    }
+    _frameBuffers[_renderBufNum][row][col] = color;
+};
+
+void Render::show() {
+//    Serial.print("Render Buf#: " + String(_renderBufNum) + "; ");
+    rp2040.fifo.push(_renderBufNum);
+//    Serial.println("free: " + String(_renderBufNum));
+    _renderBufNum = -1;
+};
