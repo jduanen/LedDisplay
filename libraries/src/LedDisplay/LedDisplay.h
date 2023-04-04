5f8f4ac05ed19a0879a1d5ebf461aafbbb40dbaa
Duane Northcutt <duane.northcutt@gmail.com>
2023-04-04 13:01:38 -0700

    Coverted from ColorLights.

--- libraries/src/LedDisplay/LedDisplay.h
+++ libraries/src/LedDisplay/LedDisplay.h
@@ -4,11 +4,17 @@
  * 
  ****************************************************************************/
 
-#ifndef _ColorLights_H_
-#define _ColorLights_H_
+#pragma once
 
 #include <Adafruit_GFX.h>
 #include "Render.h"
+
+/*
+//// TMP TMP TMP
+#define NUM_BUFS            3
+#define NUM_COLS            ((8 + 8 + 1) * 5)
+#define NUM_ROWS            7
+*/
 
 
 // for backwards client compatability
@@ -16,17 +22,14 @@
 #define WHITE       AMBER_COLORLIGHTS     ///< Draw 'on' pixels
 
 
-class ColorLights: public Adafruit_GFX, Render {
+class LedDisplay: public Adafruit_GFX, public Render {
 public:
     // NEW CONSTRUCTORS -- recommended for new projects
-    ColorLights(uint8_t w=NUM_ROWS, uint8_t h=NUM_COLS);
-    ~ColorLights(void);
+    LedDisplay(uint8_t bpp=BPP, uint8_t w=NUM_ROWS, uint8_t h=NUM_COLS);
+    ~LedDisplay(void);
+
+    void drawPixel(int16_t x, int16_t y, uint16_t color);
 
 protected:
-    uint8_t _bpp = 2;  // Bits per pixel color for this display
+    uint8_t _bpp = BPP;  // Bits per pixel color for this display
 };
-
-
-#include "ColorLights.hpp"
-
-#endif // _ColorLights_H_
