46399227029ec079db881d5c7fee6b552be9820d
Duane Northcutt <duane.northcutt@gmail.com>
2023-04-03 17:01:08 -0700

    Renamed.

--- libraries/src/LedDisplay/ColorLights.h
+++ libraries/src/LedDisplay/ColorLights.h
@@ -8,29 +8,25 @@
 #define _ColorLights_H_
 
 #include <Adafruit_GFX.h>
+#include "Render.h"
 
-#include "LedDisplay_Render.h"
-
-
-#define NUM_COLS            ((8 + 8 + 1) * 5)
-#define NUM_ROWS            7
 
 // for backwards client compatability
 #define BLACK               BLACK_COLORLIGHTS     ///< Draw 'off' pixels
 #define WHITE               AMBER_COLORLIGHTS     ///< Draw 'on' pixels
 
 
-class ColorLights: public Adafruit_GFX, LedDisplay_Render {
+class ColorLights: public Adafruit_GFX, Render {
 public:
     // NEW CONSTRUCTORS -- recommended for new projects
     ColorLights(uint8_t w=NUM_ROWS, uint8_t h=NUM_COLS);
-
     ~ColorLights(void);
-
-    void drawPixel(int16_t x, int16_t y, uint16_t color);
 
 protected:
     uint8_t _bpp = 2;  // Bits per pixel color for this display
 };
+
+
+#include "ColorLights.hpp"
 
 #endif // _ColorLights_H_
--- libraries/src/LedDisplay/ColorLights.hpp
+++ libraries/src/LedDisplay/ColorLights.hpp
@@ -15,7 +15,3 @@ ColorLights::ColorLights(uint8_t w, uint8_t h): Adafruit_GFX(w, h), _bpp(bpp) {
 ColorLights::~ColorLights(void) {
     Serial.println("Destroy");
 };
-
-void ColorLights::drawPixel(int16_t x, int16_t y, uint16_t color) {
-    _frameBuffers[_bufferNum][y][x] = color;
-};
